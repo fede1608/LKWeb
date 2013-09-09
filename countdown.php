99f10dafc19ca5141f43d01da6cda6bf10a7b851
Juan Molina <juanignaciomln@gmail.com>
2013-09-09 19:47:31 -0300

    Se agrega archivo página countdown.php para evitar la página de "en construcción" feita que teníamos.

---
+++ countdown.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Ya viene....</title>
+		<meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+		<link rel="stylesheet" href="css/animate.css" type="text/css" />
+		<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/font.css" type="text/css" cache="false" />
+		<link rel="stylesheet" href="css/plugin.css" type="text/css" />
+		<link rel="stylesheet" href="css/app.css" type="text/css" />
+        <link rel="stylesheet" href="css/lkcss.css">
+        <link href='http://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+		<!--[if lt IE 9]>
+			<script src="js/ie/respond.min.js" cache="false">
+			</script>
+			<script src="js/ie/html5.js" cache="false">
+			</script>
+			<script src="js/ie/fix.js" cache="false">
+			</script>
+		<![endif]-->
+	</head>
+	<body style=" background-image:url(images/wallpaper_freya_d1920x1200.jpg) !important; background-size:cover !important;">
+		
+        <!-- Conexión con facebook -->
+        <div id="fb-root"></div>
+		<script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=438576089566191";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+         <!-- / Conexión con facebook -->
+        
+        <section id="content" class="wrapper-md animated fadeInUp">
+        
+        <!-- Logo -->
+        <div class="row m-n text-center">
+        	<img src="images/logo.png" width="523" height="161">
+        </div>
+        <div class="row m-n">
+        <h1 class="letras-blancas" style=" text-align:center; font-family:Alegreya SC; margin-top:0; margin-bottom:14px; font-size: 36px;">
+        Vuelve el servidor de Lineage 2 Freya líder de América Latina.<br> Vuelve Linekkit.<br> <b>Volvés vos.</b> <br><br>
+        Apertura: Miércoles 18/09/13, a las 20:00 Hs (ARG)
+        <br>
+        
+        <span style="font-size:18px">
+XP x10 | SP x10 | Drop x7 | Spoil x7 | Adena x10 | Party x1.5 | Raid drop x3 | Quest drop x3 | DualBox (hasta 2 clientes por PC)
+		<br>
+¿Querés saber más? La comunidad y nuestro equipo te esperan en el foro <br>
+<a class="letras-blancas" style="font-size:36px" href="http://linekkit.com/forum/index.php">
+Visitar el foro</a>
+		<br>
+        </span>
+        
+        </h1>
+        </div>
+        
+        
+		</section>
+        <div class="row m-n"><div class="col-lg-4 col-lg-offset-2" style="
+        "><a href="http://vgw.hopzone.net/site/vote/95232/1" target="_blank" class="pull-right
+        "><img src="http://linekkit.com/images/hopzone.png" alt="Votar por Linekkit"></a></div><div class="col-lg-4" style="
+        "><a rel="nofollow" target="_blank" href="http://l2topzone.com/vote.php?id=9744" title="Lineage 2 Servers" class="pull-left"><img src="http://linekkit.com/images/topzone.png" alt="Votar por Linekkit"></a>
+        
+        </div></div>
+		<!-- footer -->
+		<footer id="footer" style="margin-top: 10px;">
+			<div class="text-center padder clearfix">
+				<p>
+					<small>
+						Linekkit Freya
+					<br>
+						linekkit.com &copy; 2013
+					</small>
+				</p>
+			</div>
+		</footer>
+		<!-- / footer -->
+	<script src="js/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+  <!-- app -->
+  <script src="js/app.js"></script>
+  <script src="js/app.plugin.js"></script>
+  <script src="js/app.data.js"></script>
+	</body>
+
+</html>
